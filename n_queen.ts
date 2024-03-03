28bdf3d0579e4a8f977b23ba9c52e9ad9515471d
Hyunsu Joo <hs9880@gmail.com>
2024-03-03 19:49:35 +0900

    Create n_queen.ts

---
+++ n_queen.ts
@@ -0,0 +1,26 @@
+def solution(n):
+    cnt = 0
+    isVisited_col = [-1 for _ in range(n)]  # i번째 행의 퀸이 위치한 열
+
+    def isValidToPutQueen(level, col):
+        for prev_level in range(level):
+            # 같은 열이나 대각선에 다른 퀸이 있는지 확인
+            if isVisited_col[prev_level] == col or \
+               level - prev_level == abs(col - isVisited_col[prev_level]):
+                return False
+        return True
+
+    def recursiveNQueen(level):
+        nonlocal cnt
+        if level == n:
+            cnt += 1  # 유효한 배치를 찾음
+            return
+        
+        for col in range(n):
+            if isValidToPutQueen(level, col):
+                isVisited_col[level] = col
+                recursiveNQueen(level + 1)
+                isVisited_col[level] = -1  # 백트래킹
+
+    recursiveNQueen(0)
+    return cnt
