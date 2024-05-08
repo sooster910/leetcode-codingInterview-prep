7a8e40060a5b30d1a311bc8c01c477f6a6c5e0cb
Hyunsu Joo <hs9880@gmail.com>
2024-05-08 14:32:06 +0900

    Time: 61 ms (13.29%), Space: 49.1 MB (22.78%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const result = []
+    for (const i in arr){
+        if(fn(arr[i],Number(i))){
+            result.push(arr[i])
+        }
+    }
+    return result
+};
