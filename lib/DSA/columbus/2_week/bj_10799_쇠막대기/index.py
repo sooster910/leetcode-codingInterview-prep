46bd03af9e4b7d2cc0b9b7d390175aa8dc8c4490
Hyunsu <hs9880@gmail.com>
2024-08-17 23:20:07 +0900

    add 백준 쇠막대기 문제 풀이 추가

--- lib/DSA/columbus/2_week/bj_10799_쇠막대기/index.py
+++ lib/DSA/columbus/2_week/bj_10799_쇠막대기/index.py
@@ -1,15 +1,34 @@
 """
+길이가 다른 여러개의 쇠막대기를 효율적으로 절단하기 위해 서로 묶어 겹쳐놓은 상태서 절단한다.
+이 때 위에서  절단 할 때 잘려진 쇠막대기 조각의 총 개수를 구하는 문제 이다. 
+
+쇠막대기와 레이저를 괄호로 나타낸다. 
+
 레이저는 여는 괄호와 닫는 괄호의 인접한 쌍으로 표현 
-또한 모든 () 는 반드시 ㄹㅔ이저를 표현한다.
+또한 모든 () 는 반드시 레이저를 표현한다.
 
-
-레이저인 경우 r로 변경한다.
-
-) 
+풀이 방식은 쌍이 있다는 점에서 힌트를 얻어 스택으로 풀 수 있다. 
 """
 
+parenthesis= input()
+
+stack=[]  
+total=0
+for i in range(len(parenthesis)): #주어진 괄호를 순회한다
+    if parenthesis[i] == "(": # 만약 괄호가 ( 오픈이라면 
+        stack.append(parenthesis[i]) # 스택에 추가 한다
+    elif parenthesis[i] == ")": # 만약 괄호가 ) 이고
+        if(parenthesis[i-1]=="("):  # 바로 직전이 ( 라면 
+            stack.pop() # () 가 되니 곧 레이저 라는 의미가 된다. 그럼 막대를 자른다. 기존 막대의 길이와는 상관이 없으므로 pop한다.
+            total+= len(stack) # 막대를 잘랐으니 total에 현재 있는 stack 을 추가한다 
+        elif(stack[-1] == "("): # 괄호가) 이고 스택의 마지막이 ( 라면
+            stack.pop()  #그냥 pop한다.
+            total+=1  #total에 1개 추가한다.
+print(total)
 
 
+
+# 1차 풀이 방식  - 레이저는 r로 치환해 풀려고 했는데, 잘 안됐음
 # parenthesis=str(input())
 # open="("
 # close=")"
@@ -28,17 +47,3 @@
 # 			#레이저가 있고 닫힌괄호라면
 # 			#레이저 개수 세고 
 
-
-from collections import deque
-
-
-linked_list =deque()
-linked_list.append(3) #append
-# del linked_list[0] # O(n)
-linked_list.remove(3). 
-
-linked_list.appendleft() #prepend
-
-linked_list.popleft() # delete_first
-linked_list.pop() # delete_last
-print(linked_list) # display
--- lib/DSA/columbus/2_week/bj_1406_에디터/index.py
+++ lib/DSA/columbus/2_week/bj_1406_에디터/index.py
@@ -1,25 +1,49 @@
-s = str(input())
+# s = str(input())
 
-m = int(input())
-cursor = len(s)
-for _ in range(m):
-	command=input().split()
+# m = int(input())
+
+# print("aa", ''.join(["x"]+["a","b","c","d","y"][::-1]))
+# cursor = len(s)
+# for _ in range(m):
+# 	command=input().split()
 	
-	print(command,s, cursor)
-	if command[0] == "P":
-		s = s[0:cursor] + command[1]+ s[cursor:]
-		cursor = max(cursor-1,0)
-	elif command[0] == "L":
-		cursor = max(0, cursor-1)
-		print("L")
-	elif command[0] =="D":
-		cursor = min(cursor+1, len(s))
-	elif command[0] == "B":
-		target_index = max(0,cursor-1)
-		s=s[0:target_index] + s[target_index+1:]
-		cursor= max(0,cursor-1)
+# 	print(command,s, cursor)
+# 	if command[0] == "P":
+# 		s = s[0:cursor] + command[1]+ s[cursor:]
+# 		if cursor ==0:
+# 			cursor = max(cursor-1,0)
+# 		else: 
+# 			cursor = min(cursor+1, len(s))
+# 	elif command[0] == "L":
+# 		cursor = max(0, cursor-1)
+# 		print("L")
+# 	elif command[0] =="D":
+# 		cursor = min(cursor+1, len(s))
+# 	elif command[0] == "B":
+# 		target_index = max(0,cursor-1)
+# 		#target_index가 0일경우는 더 이상 지울게 없다.
+# 		if(target_index != 0):	 # target_index가 0 이 아닐 경우에만 
+# 			s=s[0:target_index] + s[target_index+1:]
+# 			cursor= max(0,cursor-1)
 
 
-print("answer",s)
+# print("answer",s)
+
+import sys
+
+
+def edit_string(initial_string,commands):
+	l_stack = 
+	return 0
+
+
+# 입력 처리
+initial_string = sys.stdin.readline().strip()
+n_commands = int(sys.stdin.readline())
+commands = [sys.stdin.readline().strip() for _ in range(n_commands)]
+
+# 결과 출력
+result = edit_string(initial_string, commands)
+print(result)
 
 
