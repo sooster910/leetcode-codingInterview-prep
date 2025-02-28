498ac8d209f1de77ee05a7dcf259d3d5e66f60b2
Hensley <hs9880@gmail.com>
2025-02-28 12:30:25 +0900

    Time: 35 ms (98.91%), Space: 56.5 MB (19.56%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var triangularSum = function(nums) {
-    //0 indexed, integer
-    while(nums.length>1){
-        const newNums = []
-        for(let i=0; i<nums.length-1; i++){
-            newNums.push((nums[i]+nums[i+1]) %10)
-
+    while(nums.length > 1) {
+        for(let i = 0; i + 1 < nums.length; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
         }
-        nums = newNums
+        nums.pop();
     }
-
-    return nums[0]
+    return nums[0];
 };
