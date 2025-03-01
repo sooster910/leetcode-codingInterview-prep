2abd833d487a9675fd3d7230341d2b288f21b8f8
Hensley <hs9880@gmail.com>
2025-03-01 15:42:08 +0900

    Time: 7 ms (14.06%), Space: 57.9 MB (6.25%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.js
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.js
@@ -6,9 +6,36 @@
 var findRotation = function(mat, target) {
     
     // check if mat, target same otherwise false
+
+
     for(let r = 0; r<4; r++){
-        //transpose 
-        for(let i=0;i<mat.length; i++){
+
+        rotate90(mat)
+        //check 
+        if(areMatrixesEqual(mat, target)) return true
+
+    }
+    return false
+        
+};
+
+function areMatrixesEqual(mat, target){
+    let isMatched= true
+    for(let i =0; i<mat.length; i++){
+            for(let j =0; j<mat[0].length; j++){
+                if( mat[i][j] !==target[i][j]){
+                    isMatched = false
+                }
+            }
+    }
+
+    if(isMatched){
+        return true
+    }
+    return false
+}
+function rotate90(mat){
+    for(let i=0;i<mat.length; i++){
             for(let j= i+1;j<mat[0].length; j++){
                 [mat[i][j], mat[j][i]]= [mat[j][i],mat[i][j]]
             }
@@ -22,22 +49,4 @@ var findRotation = function(mat, target) {
             }
         }
 
-             //check 
-        let isMatched = true
-        for(let i =0; i<mat.length; i++){
-            for(let j =0; j<mat[0].length; j++){
-                if( mat[i][j] !==target[i][j]){
-                    isMatched = false
-                }
-            }
-        }
-        if(isMatched){
-            return true
-        }
-
-    }
-    
-    return false
-        
-
-};
+}
